a835617c868f157f124273b914db45219ada4421
imane <benbenimane@hotmail.com>
2020-04-03 15:30:12 +0200

    Updates

---
+++ precache-manifest.f5b88bd2150fd1339fed2ce1aa66d146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086f3f31423afb3686d1b95322f071fe",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "b35299567c1bfb1afca2",
+    "url": "/to-do/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8148acb5bfc0607d5f8",
+    "url": "/to-do/static/js/2.471bf600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do/static/js/2.471bf600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35299567c1bfb1afca2",
+    "url": "/to-do/static/js/main.3b5ab227.chunk.js"
+  },
+  {
+    "revision": "0ee5dd34f323d18ccaf7",
+    "url": "/to-do/static/js/runtime-main.a2024c7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
